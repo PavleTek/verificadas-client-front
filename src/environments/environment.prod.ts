ab059b9cae7ff49f299d3fb3695eba29e7febc05
Pavle <pavle@pavletek.com>
2024-04-17 18:17:27 -0400

    production values changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     noDisplayValue: 'No Especificar',
-    baseUrl: 'http://localhost:3000',
-    baseImageUrl: 'http://localhost:3000/images',
-    basePendingimageUrl: 'http://localhost:3000/pending-images',
+    baseUrl: 'http://api.verificadas.cl',
+    baseImageUrl: 'http://api.verificadas.cl/images',
+    basePendingimageUrl: 'http://api.verificadas.cl/pending-images',
   };
   
