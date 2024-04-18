201253ce1f083953e34f9a3a00fd76ef537e216b
Pavle <pavle@pavletek.com>
2024-04-18 19:12:16 -0400

    api to https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     noDisplayValue: 'No Especificar',
-    baseUrl: 'http://localhost:3000',
-    baseImageUrl: 'http://localhost:3000/images',
-    basePendingimageUrl: 'http://localhost:3000/pending-images',
+    baseUrl: 'https://api.verificadas.cl',
+    baseImageUrl: 'https://api.verificadas.cl/images',
+    basePendingimageUrl: 'https://api.verificadas.cl/pending-images',
   };
   
