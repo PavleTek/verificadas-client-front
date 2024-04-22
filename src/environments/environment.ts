d3b3e0061fa4bcbecf614f7654017c2a5ca08b26
Pavle <pavle@pavletek.com>
2024-04-22 12:35:02 -0400

    a bit better on phone, and girl page matches the original page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
-export const environment = {
-  production: false,
-  noDisplayValue: 'No Especificar',
-  baseUrl: 'http://localhost:3000',
-  baseMultimediaUrl: 'http://localhost:3100',
-  baseImageUrl: 'http://localhost:3100/images',
-  basePendingimageUrl: 'http://localhost:3100/pending-images',
-};
-
 // export const environment = {
-//   production: true,
+//   production: false,
 //   noDisplayValue: 'No Especificar',
-//   baseMultimediaUrl: 'https://cdn.verificadas.cl',
-//   baseUrl: 'https://api.verificadas.cl',
-//   baseImageUrl: 'https://cdn.verificadas.cl/images',
-//   basePendingimageUrl: 'https://cdn.verificadas.cl/pending-images',
+//   baseUrl: 'http://localhost:3000',
+//   baseMultimediaUrl: 'http://localhost:3100',
+//   baseImageUrl: 'http://localhost:3100/images',
+//   basePendingimageUrl: 'http://localhost:3100/pending-images',
 // };
+
+export const environment = {
+  production: true,
+  noDisplayValue: 'No Especificar',
+  baseMultimediaUrl: 'https://cdn.verificadas.cl',
+  baseUrl: 'https://api.verificadas.cl',
+  baseImageUrl: 'https://cdn.verificadas.cl/images',
+  basePendingimageUrl: 'https://cdn.verificadas.cl/pending-images',
+};
